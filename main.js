07b6be4748bd3880107919efc5f754700afa7c02
Muhid4life <aabdu007@uottawa.ca>
2018-07-16 16:33:01 -0400

    Changed Bootstrap version and updated main.js

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
+
+function deleteIssue(id){
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for(var i=0; i < issues.legnth; i++){
+        if(issues[i].id == id){
+            issues.splice(i,1);
+        }
+    }
+    // localStorage.setItem('key', 'value');
+    localStorage.setItem('issues',JSON.stringify(issues));
+    fetchIssues();
+    
+}
+
+function setStatusClosed(id){
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for(var i=0; i < issues.length;i++){
+        if (issues[i].id == id){
+            issues.status = 'Closed';
+        }
+    }
+    
+    localStorage.setItem('issues',JSON.stringify(issues));
+    fetchIssues();
+}
+
+/* function setStatusOpen(id){
+    var issues = JSOn.parse(localStorage.getItem('Issues'));
+
+    for(var i=0; i < issues.length;i++){
+        if (issues[i].id ==id){
+            issues.status = 'Open';
+        }
+    }
+
+    localStorage.setItem(issues,JSON.stringify(issues));
+    fetchIssues();
+} */
+
 function saveIssue(e){
     var issueDesc = document.getElementById('issueDescInput').value;
     var issueSeverity = document.getElementById('issueSeverityInput').value;
@@ -52,11 +93,10 @@ function fetchIssues(){
                                 '<h6>Issue ID: ' + id + '</h6>'+
                                 '<p><span class="label label-info">'+ status +'</span></p>'+
                                 '<h3>' + desc + '</h3>'+
-                                '<p><span class="glyphicon glyphicon-time"></span' + severity + '</p>'+
-                                '<p><span class="glyphicon glyphicon-time"></span' + assignedTo + '</p>'+
+                                '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>'+
+                                '<p><span class="glyphicon glyphicon-time"></span>' + assignedTo + '</p>'+
                                 '<a href="#" onClick ="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>'+
                                 '<a href="#" onClick ="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
                                 '</div>';
     }
-
 }
